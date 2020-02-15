708131b7e4a4cf785c4d0eed60987f018b33edba
BOB <schlagadam@gmail.com>
2020-02-15 10:17:42 -0600

    Moss Giant Can move between 2 points back and forth.

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MossGiant : Enemy
 {
+    private Vector3 _currentTarget;
 
     public void Start()
     {
-        Attack();
-        
+
     }
 
     public override void Update()
     {
-        Debug.Log("Moss Giant Updated");
+        Movement();
     }
+
+    void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed*Time.deltaTime);
+
+    }
+
 }
