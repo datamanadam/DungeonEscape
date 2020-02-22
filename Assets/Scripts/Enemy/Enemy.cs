813e94a772fc38bb2ea06e41d8f9d3c1d75ee663
BOB <schlagadam@gmail.com>
2020-02-22 07:58:26 -0600

    Combined Spider and Moss giant in to one script of enemy. POLYMORPHISM AND INHERITENCE.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,8 +10,55 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected int gems;
     [SerializeField] protected Transform pointA, pointB;
 
-    public abstract void Update();
+    protected Vector3 currentTarget;
+    protected Animator animator;
+    protected SpriteRenderer sprite;
 
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void Start()
+    {
+        Init();
+        currentTarget = pointA.position;
+    }
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) { return; }
+        Movement();
+    }
+
+
+    public virtual void Movement()
+    {
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
 
 
 }
--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,53 +4,10 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
-    private Vector3 _currentTarget;
-    private Animator _anim;
-    private SpriteRenderer _spiderSprite;
-
-
-    private void Start()
+    public override void Init()
     {
-        _spiderSprite = GetComponentInChildren<SpriteRenderer>();
-        _anim = GetComponentInChildren<Animator>();
-        _currentTarget = pointA.position;
+        base.Init();
     }
 
-
-
-    public override void Update()
-    {
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")) { return; }
-        Movement();
-    }
-
-    void Movement()
-    {
-
-        if (_currentTarget == pointA.position)
-        {
-            _spiderSprite.flipX = true;
-        }
-        else
-        {
-            _spiderSprite.flipX = false;
-        }
-
-
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-
-    }
 
 }
