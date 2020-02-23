453df12e4e0a4c35ac414cf2776067df7d42f00c
BOB <schlagadam@gmail.com>
2020-02-23 07:55:29 -0600

    Skeleton Enemy Set Up.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,7 +6,7 @@ public abstract class Enemy : MonoBehaviour
 {
 
     [SerializeField] protected int health;
-    [SerializeField] protected int speed;
+    [SerializeField] protected float speed;
     [SerializeField] protected int gems;
     [SerializeField] protected Transform pointA, pointB;
 
