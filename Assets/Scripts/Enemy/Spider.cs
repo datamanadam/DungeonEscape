c04639a27d594c458d575524639bdab0df78bff8
BOB <schlagadam@gmail.com>
2020-03-24 14:43:46 -0500

    Spider Attack and Kill Set up.

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Spider : Enemy,IDamageable
 {
+
+    public GameObject acidEffectPrefab;
     public int Health { get; set; }
 
     public override void Init()
     {
         base.Init();
+        Health = health;
     }
 
     public void Damage()
     {
+        Health--;
+        if (Health <1)
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    public override void Movement()
+    {
+        //Sit Still
+    }
+
+    public void Attack()
+    {
+        Instantiate(acidEffectPrefab,transform.position,Quaternion.identity);
     }
 
 
