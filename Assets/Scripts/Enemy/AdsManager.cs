4ba7c2377fb5789139333cc6d5e5571e540aa02d
BOB <schlagadam@gmail.com>
2020-04-19 09:22:53 -0500

    Unity Ad Logic Implmented.

---
+++ Assets/Scripts/Enemy/AdsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions
+            {
+                resultCallback = HandleShowResult
+            };
+            Advertisement.Show("rewardedVideo", options);
+        }
+
+    }
+
+    void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("You Skipped the Ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("AD FAILED");
+                break;
+
+
+        }
+    }
+
+
+}
