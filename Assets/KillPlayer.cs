08947763df9a0c6611cd0e8e398e1b35cdd51b6f
BOB <schlagadam@gmail.com>
2020-04-21 14:57:10 -0500

    Added Player Killer if player falls through world or an area without a collider. Added obstacles and Player Killer script to kill player if he lands on spikes (Obstacles).

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    private Player player;
+    private int healthBeforeFalling;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        player = other.GetComponent<Player>();
+        healthBeforeFalling = player.Health;
+        print(player.Health);
+
+        for (int x=0; x< healthBeforeFalling; x++)
+        {
+            print(player.Health);
+            player.Damage();
+        }
+
+    }
+
+}
